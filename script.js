4171a9ed001dbed6700281d8a6e68c3ee01171c0
Ashon Woodbury <a.woodbury01@gmail.com>
2020-08-14 10:59:07 -0400

    uploading to gh

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-fetch("http://localhost:8088/food")
-    .then(foods => foods.json())
-    .then(parsedFoods => {        
-        parsedFoods.forEach(myList(Linguine, 1))
-            console.log(myList)
-                        
-           function myList(item, index) {
-            // Print foods to DOM
-            document.querySelector("#content-container").innerHTML += index + ":" + item + "<br>"
+function printRestaurant (restaurantName){
+    return `
+    `<p>${restaurantName}</p>``
+}
 
-           }
-           
-            // HTML container
-        //     let foodList = (list) => {
-        //     return `
-        //     <h2>Food List</h2>
-
-        //     <div id="food-container">
-        //         ${list}
-        //     </div>
-        //     `
-        // }
-
-    })     
-    
+fetch('http://localhost:3000/restaurants')
+.then(r => r.json())
+.then(restaurants => {
+    restaurants.forEach(element => {
+        // If statement matching user search to restaurant name            
+        document.
+        querySelector("#content-container").
+        innerHTML += 
+    })})
